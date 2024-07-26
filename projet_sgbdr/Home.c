45d4b6e9cebb3593d0add7a58e859584229e6ee6
kabwit <21km110@gmail.org>
2024-07-26 08:40:44 +0100

    the commit again

--- projet_sgbdr/Home.c
+++ projet_sgbdr/Home.c
@@ -4,6 +4,31 @@
 
 #define MAX_CHAMPS 10
 #define MAX_LIGNES 100
+typedef enum {ENTIER, CHAINE} TypeChamp;
+
+typedef struct {
+    char nom[50];
+    TypeChamp type;
+} Champ;
+
+typedef struct {
+    char nom[50];
+    Champ champs[MAX_CHAMPS];
+    int nombreChamps;
+    void* donnees[MAX_LIGNES][MAX_CHAMPS];
+    int nombreLignes;
+} Table;
+
+Table* creerTable(char* nom);
+void ajouterChamp(Table* table, char* nomChamp, TypeChamp type);
+void afficherTable(Table* table);
+void insererDonnee(Table* table, void* donnees[]);
+
+void insererDonnee(Table* table, void* donnees[]) {
+    if (table->nombreLignes < MAX_LIGNES) {
+        for (int i = 0; i < table->nombreChamps; i++) {
+            if (table->champs[i].type == ENTIER) {
+
 
 
 
