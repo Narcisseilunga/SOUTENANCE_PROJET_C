565e3e9c025fc3cb35ed08f2ce2285aaca04261d
kabwit <21km110@gmail.org>
2024-07-29 13:48:17 +0100

    j y ajoute les fon
    ctionalites qui manquent

--- projet_sgbdr/src/sgbdr.h
+++ projet_sgbdr/src/sgbdr.h
@@ -22,3 +22,13 @@ typedef struct {
 } Table;
 
 void create_table(char *name, Column *columns, int column_count);
+void insert_data(char *table_name, void **data);
+void select_data(char *table_name, char *condition);
+void display_data(char *table_name);
+void delete_data(char *table_name, char *condition);
+void update_data(char *table_name, char *condition, void **new_data);
+void sort_data(char *table_name, char *column_name);
+void join_tables(char *table1_name, char *table2_name, char *join_column);
+void interpret_command(char *command);
+
+#endif
