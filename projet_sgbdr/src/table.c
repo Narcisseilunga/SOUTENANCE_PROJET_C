59f6c6f4f6b744d6dc2f7e8fa0e68d2688e8c251
kabwit <21km110@gmail.org>
2024-07-29 13:53:19 +0100

    les fonctions sont bien creer a present

--- projet_sgbdr/src/table.c
+++ projet_sgbdr/src/table.c
@@ -17,3 +17,21 @@ void create_table(char *name, Column *columns, int column_count) {
     tables[table_count].row_count = 0;
     
     for (int i = 0; i < column_count; i++) {
+                strcpy(tables[table_count].columns[i].name, columns[i].name);
+        tables[table_count].columns[i].type = columns[i].type;
+    }
+    
+    tables[table_count].data = (void **)malloc(MAX_COLUMNS * MAX_STRING_LENGTH * sizeof(void *));
+    table_count++;
+    printf("Table '%s' créée avec succès.\n", name);
+}
+
+Table* find_table(char *name) {
+    for (int i = 0; i < table_count; i++) {
+        if (strcmp(tables[i].name, name) == 0) {
+            return &tables[i];
+        }
+    }
+    return NULL;
+}
+
